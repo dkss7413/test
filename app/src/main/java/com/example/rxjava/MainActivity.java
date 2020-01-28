a80c6dee412da60410cbbf97f2ac4cfa9d3ee845
dkss7413 <45916438+dkss7413@users.noreply.github.com>
2020-01-28 17:59:41 +0900

    test#	new file:   app/.gitignore

---
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Observable.just(1, 2, 3, 4, 5)
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        return integer % 2 == 0;
+                    }
+                });
+
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+}
